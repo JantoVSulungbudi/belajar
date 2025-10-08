6b0b2c4e457513bbe47f8e593b1e47f037a1ee4a
JantoVSulungbudi <8272501001@student.unpar.ac.id>
2025-10-08 13:42:32 +0700

    Add files via upload

---
+++ STEM.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/search')
+def search():
+  query = request.args.get('query')
+  return f'Kamu mencari: {query}'
+  
+if __name__ == '__main__':
+  app.run(debug=True)
